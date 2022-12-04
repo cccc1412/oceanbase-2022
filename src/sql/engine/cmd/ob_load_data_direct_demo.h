19c6e7501ce2c077ab21972959a0b5f38d090970
zzz <827909371@qq.com>
2022-12-04 22:29:10 +0800

    sort buf

--- src/sql/engine/cmd/ob_load_data_direct_demo.h
+++ src/sql/engine/cmd/ob_load_data_direct_demo.h
@@ -24,7 +24,7 @@ static const int64_t FILE_BUFFER_SIZE = (2LL << 20);        // 2M
 static const int64_t SAMPLING_NUM = (1LL << 20);            // 1M
 static const int64_t BUFFER_NUM = (4LL << 20);              // 4M
 
-static const uint64_t SLEEP_TIME = 500000;
+static const uint64_t SLEEP_TIME = 200000;
 
 class ObLoadDataBuffer {
 public:

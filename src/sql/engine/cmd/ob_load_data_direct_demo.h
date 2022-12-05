1045587ceaa234d55b1174e3dd6b43a62686e0d9
zzz <827909371@qq.com>
2022-12-05 11:07:44 +0800

    sort buf

--- src/sql/engine/cmd/ob_load_data_direct_demo.h
+++ src/sql/engine/cmd/ob_load_data_direct_demo.h
@@ -18,11 +18,11 @@
 namespace oceanbase {
 namespace sql {
 
-static const int64_t MAX_RECORD_SIZE = (1LL << 12);         // 4K
-static const int64_t MEM_BUFFER_SIZE = (1LL << 20) * 540LL; // 540M
-static const int64_t FILE_BUFFER_SIZE = (2LL << 20);        // 2M
-static const int64_t SAMPLING_NUM = (1LL << 20);            // 1M
-static const int64_t BUFFER_NUM = (4LL << 20);              // 4M
+static const int64_t MAX_RECORD_SIZE = (1LL << 12);          // 4K
+static const int64_t MEM_BUFFER_SIZE = (1LL << 20) * 512LL;  // 540M
+static const int64_t FILE_BUFFER_SIZE = (2LL << 20);         // 2M
+static const int64_t SAMPLING_NUM = (1LL << 20);             // 1M
+static const int64_t BUFFER_NUM = (3LL << 20) + (6LL << 19); // 4M
 
 static const uint64_t SLEEP_TIME = 200000;
 

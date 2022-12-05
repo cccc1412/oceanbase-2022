8266f045b2703765c0aa94933532794830d112f9
cccc1412 <674987879@qq.com>
2022-12-05 17:18:00 +0800

    temp

--- src/sql/engine/cmd/ob_load_data_executor.h
+++ src/sql/engine/cmd/ob_load_data_executor.h
@@ -22,8 +22,8 @@ namespace sql
 {
 class ObLoadDataStmt;
 class ObLoadDataExecutor {
-  static const int64_t PROCESS_THREAD_NUM = 8;
-  static const int64_t LOAD_THREAD_NUM = 16;
+  static const int64_t PROCESS_THREAD_NUM = 7;
+  static const int64_t LOAD_THREAD_NUM = 11;
 public:
   ObLoadDataExecutor() {}
   virtual ~ObLoadDataExecutor() {}
